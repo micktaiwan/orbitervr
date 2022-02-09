546e3437f2add62320c60d738b156234bb566a8b
Mickael Faivre-Maçon <micktaiwan@users.noreply.github.com>
2022-02-09 12:48:11 +0100

    eslint

--- client/main.js
+++ client/main.js
@@ -1,20 +1,13 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.scene.onCreated(function helloOnCreated() {
+Template.scene.onCreated(() => {
 });
 
 Template.scene.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
 });
 
 Template.scene.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
 });
