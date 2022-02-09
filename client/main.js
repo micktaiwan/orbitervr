67aee0f7f36211c90de63c18c4146a88c192ba62
Mickael Faivre-Maçon <micktaiwan@users.noreply.github.com>
2022-02-09 12:06:14 +0100

    first scene

--- client/main.js
+++ client/main.js
@@ -3,18 +3,16 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.scene.onCreated(function helloOnCreated() {
 });
 
-Template.hello.helpers({
+Template.scene.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.scene.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
